64d33872695f56bad550992d75165f22fa27caa2
loja5306 <lukeatkinson0601@gmail.com>
2025-11-26 21:26:03 +0000

    Trying to fix Not Found error with changing Browser Router

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import NotFound from "./pages/NotFound";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/Portfolio-Website">
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
